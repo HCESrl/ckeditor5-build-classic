5266fdb0faf4a33f55b95a49c5b245431110e4f3
fsavina <fabio.savina@gmail.com>
2019-01-28 18:15:35 +0100

    Add text alignment

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -8,17 +8,18 @@ import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classicedi
 
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
 import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
-// import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
+import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
 import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
 import Subscript from '@ckeditor/ckeditor5-basic-styles/src/subscript';
 import Superscript from '@ckeditor/ckeditor5-basic-styles/src/superscript';
-// import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
+import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
 import Font from '@ckeditor/ckeditor5-font/src/font';
 // import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
 // import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading';
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
 import Image from '@ckeditor/ckeditor5-image/src/image';
 import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
 import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
@@ -28,7 +29,7 @@ import Link from '@ckeditor/ckeditor5-link/src/link';
 import List from '@ckeditor/ckeditor5-list/src/list';
 import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
-// import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
+import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 
@@ -38,14 +39,15 @@ export default class ClassicEditor extends ClassicEditorBase {}
 ClassicEditor.builtinPlugins = [
 	Essentials,
 	UploadAdapter,
-	// Autoformat,
+	Autoformat,
 	Bold,
 	Italic,
 	Underline,
 	Subscript,
 	Superscript,
-	// BlockQuote,
+	BlockQuote,
 	Font,
+	Alignment,
 	// CKFinder,
 	// EasyImage,
 	Heading,
@@ -58,7 +60,7 @@ ClassicEditor.builtinPlugins = [
 	List,
 	MediaEmbed,
 	Paragraph,
-	// PasteFromOffice,
+	PasteFromOffice,
 	Table,
 	TableToolbar
 ];
@@ -67,9 +69,9 @@ ClassicEditor.builtinPlugins = [
 ClassicEditor.defaultConfig = {
 	toolbar: {
 		items: [
-			'heading', 
+			'heading', 'fontSize', 'alignment',
 			'|',
-			'fontSize', 'bulletedList', 'numberedList',
+			'bulletedList', 'numberedList',
 			'|',
 			'bold', 'italic', 'underline', 
 			'|',
